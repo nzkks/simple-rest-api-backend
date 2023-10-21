4e17454dbc173685531c2ab6b68d02dbb3cea26f
Shanthosh Krishnakumar <kkshanthosh@gmail.com>
2023-10-22 11:22:08 +1300

    Create a server with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello typescript!');
+import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import http from 'http';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:8080/');
+});
